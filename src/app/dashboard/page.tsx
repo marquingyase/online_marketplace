74ae6816610d6f0b87bcbadda362473a6facccad
Marquin GyaSe <67365881+marquingyase@users.noreply.github.com>
2025-07-17 22:10:47 +0000

    Dashboard folder

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Container from "@/components/Container";
+
+
+export default function Dashboard() {
+    return (
+        <main>
+            <Container>Dashboard</Container>
+        </main>
+    );
+}
